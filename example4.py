bbe674a6bde83f7c6bc68dc96cf4d8a714ee1f6c
Pepe García <pepe@pepegar.com>
2021-02-11 13:54:56 +0100

    add code from class

--- example4.py
+++ example4.py
@@ -10,13 +10,25 @@ app = dash.Dash(__name__)
 
 districts = [{"label": district, "value": district} for district in  df["DISTRITO"].unique()]
 
+kinds_of_accidents = []
+for kind in df["TIPO ACCIDENTE"].unique():
+    kinds_of_accidents.append({"label": kind, "value": kind})
+
 app.layout = html.Div(children = [
     html.H1("Bicimad accidents by district"),
+    html.H2("District"),
     dcc.Dropdown(
         id="district",
         options=districts,
         multi=True,
         value=[districts[0]["value"]]
+    ),
+    html.H2("kind of accident"),
+    dcc.Dropdown(
+        id="accident-kind",
+        options=kinds_of_accidents,
+        multi=True,
+        value=[kinds_of_accidents[0]["value"]]
     ),
     dcc.Graph(
         id="accidents-graph",
@@ -33,14 +45,20 @@ xs = list(sorted(df["HORA"].unique()))
 
 @app.callback(
     Output(component_id="accidents-graph",component_property="figure"),
-    [Input(component_id="district", component_property="value")]
+    [
+     Input(component_id="district", component_property="value"),
+     Input(component_id="accident-kind", component_property="value")
+     ]
 )
-def update(districts):
+def update(districts, kinds_of_accidents):
 
     data = []
+    
+    filtered = df["TIPO ACCIDENTE"].isin(kinds_of_accidents)
+    filtered_df = df[filtered]
 
     for district in districts:
-        count = df[df["DISTRITO"] == district]["HORA"].count()
+        count = filtered_df[filtered_df["DISTRITO"] == district]["HORA"].count()
         row = {'x': list(range(0, 100)), 'y': [count], 'type': 'bar', 'name': district}
         data.append(row)
 
