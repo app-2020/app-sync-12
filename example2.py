1f216047230efde6b73db6561622f09fa9827cd8
Pepe García <pepe@pepegar.com>
2021-02-11 10:17:27 +0100

    update example2...

--- example2.py
+++ example2.py
@@ -11,8 +11,8 @@ app.layout = html.Div(children = [
   dcc.Dropdown(
       id="dropdown",
       options= [
-          {"value": "whatever2", "label": "one"},
-          {"value": "whatever", "label": "two"}
+          {"value": "one", "label": "one"},
+          {"value": "two", "label": "two"}
       ]),
   html.H2(id="id-changes")
 ])
@@ -21,7 +21,10 @@ app.layout = html.Div(children = [
   Output(component_id="id-changes",component_property="children"),
   [Input(component_id="dropdown", component_property="value")]
 )
-def change_h2_on_input_change(potato):
-    return "the value of the input is: " + potato
+def change_h2_on_input_change(value):
+    if value:
+        return "the value of the input is: " + value
+    else:
+        return "no value selected yet"
 
 app.run_server(debug=True)
